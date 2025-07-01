d6a6010f19e4ac303a03cf726c7e9390d1710662
Balkaran Singh <balkaransingh@khalsa.com>
2025-07-01 16:34:24 +1200

    LoRA adapter in text encoder

--- encoder/text_encoder.py
+++ encoder/text_encoder.py
@@ -1,14 +1,22 @@
 import torch
 import torch.nn as nn
 from transformers import AutoModel
+from peft import LoraConfig, get_peft_model, TaskType
 
 class TextEncoder(nn.Module):
-    def __init__(self, model_name, model_dim=128, dropout_rate=0.2, output_type='mean'):
+    def __init__(self, model_name, model_dim=128, dropout_rate=0.2, output_type='mean',
+                 use_lora=False, lora_r=8, lora_alpha=16, lora_dropout=0.05):
         """
         output_type: one of ['cls', 'mean', 'token']
         """
         super().__init__()
         self.encoder = AutoModel.from_pretrained(model_name)
+        if use_lora == True:
+            peft_config = LoraConfig(task_type=TaskType.SEQ_CLS, inference_mode=False,
+            r=lora_r, lora_alpha=lora_alpha, lora_dropout=lora_dropout,
+            target_modules=["query", "value"])
+            self.encoder = get_peft_model(self.encoder, peft_config)        
+
         hidden_size = self.encoder.config.hidden_size
         self.common_projection = nn.Linear(hidden_size, model_dim)
         self.dropout = nn.Dropout(dropout_rate)
